8ae47be60795ff58df172711f08128d3770042e7
salam <salam1313@gmail.com>
2024-09-26 19:50:10 +0530

    Initial commit

---
+++ customreact.js
@@ -0,0 +1,29 @@
+function customRender(ReactElement,container){
+    /*
+    const domElement = document.createElement(ReactElement.type)
+    domElement.innerHTML = ReactElement.children
+    domElement.setAttribute('href' , ReactElement.props.href)
+    domElement.setAttribute('target',ReactElement.props.target)
+    container.appendChild(domElement)
+    */
+  const domElement= document.createElement(ReactElement.type)
+  domElement.innerHTML = ReactElement.children
+  for (const prop in ReactElement.props) {
+   if(prop == 'children')continue;
+   domElement.setAttribute(prop,ReactElement.props[prop ])
+  }
+    container.appendChild(domElement)
+}
+
+
+const ReactElement ={
+    type :'a',
+    props :{
+        href:'https://www.google.com',
+        target : '_blank'
+    
+    },
+    children : 'Click to me to visit google'
+}
+const maincontainer = document.querySelector('#root')
+customRender(ReactElement,maincontainer)
